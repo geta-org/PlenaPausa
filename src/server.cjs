42fdf0c4541fc0e1de06c8b6b51e1222629a626a
bernrdo <bernardogbrandao@gmail.com>
2024-12-15 21:10:46 -0300

    Deploy init

---
+++ src/server.cjs
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para teste
+app.get('/health', (req, res) => {
+  res.send('Server is up and running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server hosted at http://localhost:${PORT}`);
+});
